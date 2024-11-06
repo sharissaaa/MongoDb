0e388212e49c44223f382701771d82b22bfc1cad
Sharissa Marian Hurtis <160829322+sharissaaa@users.noreply.github.com>
2024-11-06 09:29:25 +0530

    basics

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/*const interval =setInterval(()=>{
+    console.log("running");
+})
+setTimeout(()=>{
+    console.log("hello");
+},2000);*/
+
+/*console.log(__filename);
+console.log(__dirname);*/
+
+const path=require("path");
+console.log(path.basename(__filename));
+
