aa2fa9d7843fafdf932947e079cc7b261530af9a
Sharissa Marian Hurtis <160829322+sharissaaa@users.noreply.github.com>
2024-11-06 09:41:53 +0530

    Created Directory

--- scripts.js
+++ scripts.js
@@ -8,6 +8,21 @@ setTimeout(()=>{
 /*console.log(__filename);
 console.log(__dirname);*/
 
-const path=require("path");
-console.log(path.basename(__filename));
+/*const path=require("path");
+console.log(path.basename(__filename));*/
 
+/*const path=require("path");
+console.log(path.extname(__filename));*/
+
+/*const path= require("path");
+console.log(path.dirname(__filename));*/
+
+const path=require("path");
+// console.log(path.join(__dirname,"api","scripts.js"));
+
+const fs=require("fs");
+fs.mkdir(path.join(__dirname,"/api"),(err)=>{
+    if(err) throw err;
+});
+
+console.log(fs)
